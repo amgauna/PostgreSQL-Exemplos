c58952976af2e498db5b3865ea7ed53530b42fd4
Ana Gauna <amgauna@gmail.com>
2021-10-29 11:29:59 -0300

    Update and rename sintaxe-create.sql to sintaxe-create.md

--- sintaxe-create.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE name
-    [ [ WITH ] [ OWNER [=] user_name ]
-           [ TEMPLATE [=] template ]
-           [ ENCODING [=] encoding ]
-           [ LC_COLLATE [=] lc_collate ]
-           [ LC_CTYPE [=] lc_ctype ]
-           [ TABLESPACE [=] tablespace ]
-           [ CONNECTION LIMIT [=] connlimit ] ]
-           
