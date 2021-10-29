b69df86602cd829568ad565f5203d3938695418f
Ana Gauna <amgauna@gmail.com>
2021-10-29 11:18:02 -0300

    Create pessoal.sql

---
+++ pessoal.sql
@@ -0,0 +1,4 @@
+CREATE TABLE pessoal (id SERIAL PRIMARY KEY, nome VARCHAR(50));
+INSERT INTO pessoal (nome) VALUES ('Maria');
+INSERT INTO pessoal (nome) VALUES ('Manuel');
+INSERT INTO pessoal (nome) VALUES ('Francisco');
